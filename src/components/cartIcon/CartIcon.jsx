9862361e486e2e579fbad372d5f78c69df35d19d
Antonio <memphis2coder@gmail.com>
2020-11-17 07:42:02 -0500

    working on bugs

--- src/components/cartIcon/CartIcon.jsx
+++ src/components/cartIcon/CartIcon.jsx
@@ -10,30 +10,22 @@ import {createStructuredSelector} from 'reselect';
 import './CartIcon.scss';
 
 // toggleCartHidden comes from redux cart.action redux folder
-const CartIcon = ({toggleCartHidden, itemCount}) => { // pass toggleCartHidden as a argument
-    return (
-        // toogleCartHidden is a function?
-        <div className="cart-icon" onClick={toggleCartHidden}> 
-            <ShoppingIcon className="shopping-icon"/>
-            <span className="item-count">{itemCount}</span>
-        </div>
-    )
-};
-
-/////////////////// REDUX AND RESELECT ///////////////////////////////////
-
-// function that toggles the cart also connects to redux action
-const mapDispatchToProps = dispatch => ({ // dispatch connects this componenet to redux
-    // return a object
-    // return a function
-    toggleCartHidden: () => dispatch(toggleCartHidden()) // dispatch makes the action happen
-});
-
-// function to show the total number of items in cart connection to cart.reducer.js
-// this is a selector & i need to use reselect to save the data so it doesnt refresh all the time
-const mapStateToProps = createStructuredSelector({
-    itemCount: selectCartItemsCount // no state is needed with selector
-});
-
-// pass in mapDispatchToProps and MapStateToProps
-export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
+    <div className='cart-icon' onClick={toggleCartHidden}>
+      <ShoppingIcon className='shopping-icon' />
+      <span className='item-count'>{itemCount}</span>
+    </div>
+  );
+  
+  const mapDispatchToProps = dispatch => ({
+    toggleCartHidden: () => dispatch(toggleCartHidden())
+  });
+  
+  const mapStateToProps = createStructuredSelector({
+    itemCount: selectCartItemsCount
+  });
+  
+  export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(CartIcon);
