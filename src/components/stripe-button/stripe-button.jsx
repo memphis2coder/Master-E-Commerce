46853df8b6132256e7fb4ceb1596656f0d29d9bf
Antonio <memphis2coder@gmail.com>
2020-11-30 21:52:36 -0500

    update title and complete strip payment

--- src/components/stripe-button/stripe-button.jsx
+++ src/components/stripe-button/stripe-button.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({price}) => {
+
     const priceForStripe = price * 100;
-    const publishableKey = pk_test_51HtB6oC5CgRatsQZi34n8TfhJdb0yaBGNc4g9AczjQVq2jgmYijeEk6YOyeLuinoFb38tDcldkj5h4bS4rCIResg00LcYRm0Mo;
+    const publishableKey = 'pk_test_51HtB6oC5CgRatsQZi34n8TfhJdb0yaBGNc4g9AczjQVq2jgmYijeEk6YOyeLuinoFb38tDcldkj5h4bS4rCIResg00LcYRm0Mo';
     
     const onToken = token => {
         console.log(token);
--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -34,10 +34,16 @@ const CheckoutPage = ({ cartItems, total }) => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <div className='total'>TOTAL: ${total}</div>
+        <div className="test-warning">
+          *Please use the following test credit card for payments*
+          <br/>
+          4242 4242 4242 4242 - Exp: 1/22 - CCV: 123
+        </div>
       <StripeCheckoutButton price={total} />
     </div>
   );
   
+  // selector
   const mapStateToProps = createStructuredSelector({
     cartItems: selectCartItems,
     total: selectCartTotal
