e69620034001dcad173cd232233404dc7858973f
Antonio <memphis2coder@gmail.com>
2020-12-09 21:24:00 -0500

    replaced constructor with hooks for sign in component

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -9,29 +9,29 @@ const SignIn = () => {
     
     const [userCredentials, setCredentials] = useState({email: '', password: ''});
     
+    const { email, password } = userCredentials;
 
     const handleSubmit = (e) => {
         e.preventDefault(); // prevent default action
-        const { email, password } = userCredentials;
     }
 
     const handleChange = (e) => {
         const { name, value } = e.target; // pulling the name and value from target
-        this.setState({ [name]: value }); // now i can use this function on both input fields
-    }
+        setCredentials({...userCredentials, [name]: value }); // now i can use this function on both input fields
+    };
 
-    render() {
+    
         return (
             <div className="sign-in">
                 <h2 className="title">I already have a account</h2>
                 <span className="title">Sign in with your email and password</span>
 
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={handleSubmit}>
                     <FormInput
                         type="email"
                         name="email"
-                        value={this.state.email}
-                        onChange={this.handleChange}
+                        value={email}
+                        onChange={handleChange}
                         label="email"
                         required
                     />
@@ -39,8 +39,8 @@ const SignIn = () => {
                     <FormInput
                         type="password"
                         name="password"
-                        value={this.state.password}
-                        onChange={this.handleChange}
+                        value={password}
+                        onChange={handleChange}
                         label="password"
                         required
                     />
@@ -53,7 +53,7 @@ const SignIn = () => {
                 </form>
             </div>
         )
-    }
+    
 }
 
 export default SignIn;
