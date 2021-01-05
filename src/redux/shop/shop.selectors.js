b1a059a86e01f0b0808254aad7117a0d07e49f5b
Antonio <memphis2coder@gmail.com>
2021-01-05 07:20:29 -0500

    working on bug

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,28 +1,32 @@
 import { createSelector } from 'reselect';
-//import memoize from 'lodash.memoize';
-
+ 
 const selectShop = state => state.shop;
-
+ 
 export const selectCollections = createSelector(
   [selectShop],
   shop => shop.collections
 );
-
+ 
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
   collections =>
     collections ? Object.keys(collections).map(key => collections[key]) : []
 );
-
+ 
 export const selectCollection = collectionUrlParam =>
   createSelector(
     [selectCollections],
     collections => (collections ? collections[collectionUrlParam] : null)
   );
-
+ 
 export const selectIsCollectionFetching = createSelector(
   [selectShop],
   shop => shop.isFetching
+);
+ 
+export const selectIsCollectionsLoaded = createSelector(
+  [selectShop],
+  shop => !!shop.collections
 );
 
 
